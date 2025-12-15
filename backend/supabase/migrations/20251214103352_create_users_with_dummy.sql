643fc7eda4ce78a2a4a9afe31b26ae167ebc5c2d
Shunto Ishiguro <shunto.ishiguro.job@gmail.com>
2025-12-15 09:33:48 +0900

    /login エンドポイントを実装し、DB接続を追加

--- backend/supabase/migrations/20251214103352_create_users_with_dummy.sql
+++ backend/supabase/migrations/20251214103352_create_users_with_dummy.sql
@@ -1,4 +1,4 @@
-insert into public.users (username, icon_url)
+insert into public.users (username, email, password, icon_url)
 values
-  ('alice', 'https://example.com/alice.png'),
-  ('bob',   'https://example.com/bob.png');
+  ('alice', 'alice@example.com', 'alicepassword', 'https://example.com/alice.png'),
+  ('bob',   'bob@example.com',   'bobpassword',   'https://example.com/bob.png');
