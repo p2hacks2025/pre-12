2099f52802fe89b524f1adcc6cb870d901984b25
K <167762624+KAZZ00777@users.noreply.github.com>
2025-12-17 21:26:31 +0900

    Fix Riverpod State errors by using legacy API and update main.dart with ProviderScope

--- mobile/lib/features/home/works_controller.dart
+++ mobile/lib/features/home/works_controller.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 
 import '../auth/auth_controller.dart';
 import 'models.dart';
--- mobile/lib/features/review/review_service.dart
+++ mobile/lib/features/review/review_service.dart
@@ -1,4 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
